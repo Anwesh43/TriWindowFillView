83cdc96788ae440105f57e683ac53a0423b5a472
Anwesh43 <anweshthecool0@gmail.com>
2020-10-10 04:50:55 +0530

    adding TriWindowFill class

--- triwindowfillview/src/main/java/com/anwesh/uiprojects/triwindowfillview/TriWindowFillView.kt
+++ triwindowfillview/src/main/java/com/anwesh/uiprojects/triwindowfillview/TriWindowFillView.kt
@@ -176,4 +176,27 @@ class TriWindowFillView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TriWindowFill(var i : Int) {
+
+        private var curr : TWFNode = TWFNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
