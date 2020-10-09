7e28072fa277bb7c6e93a5b069220dec995c2a10
Anwesh43 <anweshthecool0@gmail.com>
2020-10-10 04:43:03 +0530

    adding TriWindowFillView class

--- triwindowfillview/src/main/java/com/anwesh/uiprojects/triwindowfillview/TriWindowFillView.kt
+++ triwindowfillview/src/main/java/com/anwesh/uiprojects/triwindowfillview/TriWindowFillView.kt
@@ -72,3 +72,19 @@ fun Canvas.drawTWFNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriWindowFill(scale, w, h, paint)
 }
+
+class TriWindowFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
