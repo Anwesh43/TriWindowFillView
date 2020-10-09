9d3cade8ba8ca4efaec3653994b1edd0918a1122
Anwesh43 <anweshthecool0@gmail.com>
2020-10-10 04:57:03 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtriwindowfillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtriwindowfillview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedtriwindowfillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.triwindowfillview.TriWindowFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriWindowFillView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
